5c4bcbc2fdfadaa89f837b67af4fa31eeba8915b
rabbitencoder <rabbitencoder@gmail.com>
2024-05-13 14:30:52 +0530

    First Commit

---
+++ src/main/java/com/rabbitencoder/restservices/SpringzeroApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitencoder.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringzeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringzeroApplication.class, args);
+	}
+
+}
