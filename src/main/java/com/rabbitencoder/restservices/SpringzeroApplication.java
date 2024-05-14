133ba80103a234e2c6c15a9781b47d4759752a43
rabbitencoder <rabbitencoder@gmail.com>
2024-05-14 17:20:04 +0530

    Added the Author name

--- src/main/java/com/rabbitencoder/restservices/SpringzeroApplication.java
+++ src/main/java/com/rabbitencoder/restservices/SpringzeroApplication.java
@@ -3,6 +3,11 @@ package com.rabbitencoder.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author rahul
+ */
+
 @SpringBootApplication
 public class SpringzeroApplication {
 
