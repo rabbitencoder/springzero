201b870797b8155c75a84bd27100231dad8dead8
rabbitencoder <rabbitencoder@gmail.com>
2025-03-23 12:29:33 +0530

    Added the Internationalization

--- src/main/java/com/rabbitencoder/restservices/SpringzeroApplication.java
+++ src/main/java/com/rabbitencoder/restservices/SpringzeroApplication.java
@@ -2,6 +2,12 @@ package com.rabbitencoder.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 
 /**
@@ -13,6 +19,20 @@ public class SpringzeroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringzeroApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver getLocaleResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource getMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
--- src/main/java/com/rabbitencoder/restservices/hello/HelloWorldController.java
+++ src/main/java/com/rabbitencoder/restservices/hello/HelloWorldController.java
@@ -1,7 +1,15 @@
 package com.rabbitencoder.restservices.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 /**
  * @author rahul
@@ -11,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 //Controller
 @RestController
 public class HelloWorldController {
+
+    private final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
     //URI - /helloworld
     //@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
     @GetMapping("helloworld1")
@@ -22,6 +35,18 @@ public class HelloWorldController {
     public UserDetails helloWorldBean(){
         return new UserDetails("rahul", "singh", "xyz");
     }
+
+    @GetMapping("/hello-int")
+    public String getMessagesInI18NFormat(@RequestHeader(name = "Accept-Language", required=false) String locale){
+        return messageSource.getMessage("label.hello", null, new Locale(locale));
+    }
+
+    @GetMapping("/hello-int2")
+    public String getMessagesInI18NFormat(){
+        return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
+    }
+
+
 }
 
     
